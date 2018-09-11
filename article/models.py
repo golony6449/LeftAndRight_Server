cc76665022d80b5e2de845e118c2d60608fbed55
golony6449 <bb0162@live.co.kr>
2018-09-12 01:04:48 +0900

    최근 기사 정보 저장을 위한 모델 수정

--- article/models.py
+++ article/models.py
@@ -11,3 +11,10 @@ class press(models.Model):
 #     press_name = models.ForeignKey(press, on_delete=models.CASCADE)
 #     user_name = model.
 #     hits_count = models.IntegerField(default=0)
+
+class recentPost(models.Model):
+    name = #TODO FK 설정
+    title = models.CharField(max_length=100)
+    keyword1 = models.CharField(max_length=20)
+    keyword2 = models.CharField(max_length=20)
+    keyword3 = models.CharField(max_length=20)
