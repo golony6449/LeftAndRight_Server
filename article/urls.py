672ef0e79c6c6adbee11be7874edc9a9df216b5a
golony6449 <ab6449@gmail.com>
2018-10-11 16:19:15 +0900

    테스트용 URL 삭제

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # path('login', views.login, name='login'),
     # path('try_login', views.try_login, name='try_login'),
-    path('test', views.test, name='test'),
     # path('register', views.register, name='register'),
     # path('register', views.RegisterView.as_view(), name='register'),
     # path('try_register', views.try_register, name='try_register'),
