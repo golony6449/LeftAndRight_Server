d9fe6da4ac1977d7a13449c4d1053cb8bb9437a2
golony6449 <ab6449@gmail.com>
2018-10-11 19:06:08 +0900

    조선일보 크롤러 수정 검색 결과 URL이 데스크톱 버전 웹으로 나오는 현상 수정

--- article/module/searchEngine.py
+++ article/module/searchEngine.py
@@ -21,24 +21,49 @@ class SearchEngine:
             Exception("WrongPressParams: Press should be chosun or hani")
 
     def _search_in_chosun(self, keyword):
-        url = 'http://search.chosun.com/search/total.search?pageconf=total&query={}'
-        html = requests.get(url.format(keyword))
+        url = 'http://m.chosun.com/svc/search.html?query={}'.format(keyword)
+        html = requests.get(url)
         html.encoding = 'utf-8'
         parser = BeautifulSoup(html.text, 'html.parser')
 
         # 관련 뉴스 검색 (지면기사, 커뮤니티 제외)
-        article_list = parser.find_all('dl', {'class': 'search_news'})[1:6]
+        article_list = parser.find_all('div', {'class': 'list_inner'})[:5]
         article_list_parsed = self._parse_article_chosun(article_list)
         return article_list_parsed
 
     def _parse_article_chosun(self, article_list):
         result = []
         for article in article_list:
-            block = article.find('dt')
-            title = block.text.strip()
+            block = article.find('div', {'class': 'tit'})
+            title = block.find('a').text
             addr = block.find('a').get('href')
-            result.append((title, addr))
+            date = block.find('span').text
+            result.append((title, addr, date))
         return result
+
+    # Legacy Code
+    # 조선일보의 데스크톱 사이트의 링크롤 반환하는 문제점 발견
+    # Solution: 모바일 페이지용 크롤러 제작
+
+    # def _search_in_chosun(self, keyword):
+    #     url = 'http://search.chosun.com/search/total.search?pageconf=total&query={}'
+    #     html = requests.get(url.format(keyword))
+    #     html.encoding = 'utf-8'
+    #     parser = BeautifulSoup(html.text, 'html.parser')
+    #
+    #     # 관련 뉴스 검색 (지면기사, 커뮤니티 제외)
+    #     article_list = parser.find_all('dl', {'class': 'search_news'})[1:6]
+    #     article_list_parsed = self._parse_article_chosun(article_list)
+    #     return article_list_parsed
+    #
+    # def _parse_article_chosun(self, article_list):
+    #     result = []
+    #     for article in article_list:
+    #         block = article.find('dt')
+    #         title = block.text.strip()
+    #         addr = block.find('a').get('href')
+    #         result.append((title, addr))
+    #     return result
 
     def _search_in_hani(self, keyword):
         url = 'http://m.hani.co.kr/arti/SEARCH/news/date/{}/list.html'.format(keyword)
